14715a1111450b32bb73a390c2a3f7806ca4f0d5
Yuri Brito <yuriabrito@gmail.com>
2017-01-22 14:18:50 -0300

    fixed topbar

--- src/components/home/topbar.js
+++ src/components/home/topbar.js
@@ -5,17 +5,19 @@ import '../../styles/topbar';
 export default (props) => {
   return (
     <topbar>
-      <topbar-content>
-        <topbar-left>
-          <img src="images/more.svg" onClick={props.signOut} />
-        </topbar-left>
-        <topbar-center>
-          Listable
-        </topbar-center>
-        <topbar-right>
-          <img src="images/search.svg" />
-        </topbar-right>
-      </topbar-content>
+      <topbar-wrap>
+        <topbar-content>
+          <topbar-left>
+            <img src="images/more.svg" onClick={props.signOut} />
+          </topbar-left>
+          <topbar-center>
+            Listable
+          </topbar-center>
+          <topbar-right>
+            <img src="images/search.svg" />
+          </topbar-right>
+        </topbar-content>
+      </topbar-wrap>
     </topbar>
   );
 };
