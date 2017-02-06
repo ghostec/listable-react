f8b08688f0863c4293f492e212f156326c8c3247
Yuri Brito <yuriabrito@gmail.com>
2017-02-06 01:27:50 -0300

    refactores topbar containers

--- src/containers/home/topbar.js
+++ src/containers/home/topbar.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import TopBar from '../../components/home/topbar';
+import TopBar from 'components/common/topbar';
+import Options from 'containers/home/options/options';
+import routes from 'constants/routes';
+import redirect from 'helpers/redirect';
+
+const Left = props => {
+  const { options, toggleOptions } = props;
+
+  return (
+    <img src="images/more.svg" onClick={event => toggleOptions(event, options)} />
+  );
+}
+
+const Center = props => {
+  const { search } = props;
+
+  return (
+    <span>Listabble</span>
+  );
+}
+
+const Right = props => {
+  const goToSearch = () => {
+    redirect(routes.generate('search'));
+  }
+
+  return (
+    <img src="images/search.svg" onClick={goToSearch} />
+  );
+}
 
 class TopBarContainer extends React.Component {
   constructor(props) {
@@ -11,7 +40,11 @@ class TopBarContainer extends React.Component {
   render() {
     const { toggleOptions, dispatch } = this.props;
 
-    return <TopBar toggleOptions={toggleOptions} dispatch={dispatch} />;
+    const options = <Options toggleOptions={toggleOptions} dispatch={dispatch} />;
+
+    return <TopBar left={<Left toggleOptions={toggleOptions} options={options} />}
+              center={<Center />}
+              right={<Right />}/>;
   }
 };
 
--- src/containers/list/topbar.js
+++ src/containers/list/topbar.js
@@ -1,26 +1,48 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import routes from '../../constants/routes';
-import TopBar from '../../components/list/topbar';
-import * as navigation from '../../actions/navigation';
+import routes from 'constants/routes';
+import TopBar from 'components/common/topbar';
+import * as navigation from 'actions/navigation';
+
+const Left = props => {
+  const { dispatch } = props;
+
+  const back = () => dispatch(navigation.backBegin(routes.generate('home')));
+
+  return (
+    <img src="images/back.svg" onClick={back} />
+  );
+}
+
+const Center = props => {
+  return (
+    <list-topbar-save>
+      <img id="list-topbar-save-ring" src="images/open-ring-blue.svg" />
+      <list-topbar-text>save</list-topbar-text>
+    </list-topbar-save>
+  );
+}
+
+const Right = props => {
+  const { n_items } = props;
+
+  return (<span>
+    {n_items != undefined && n_items > 0 && <list-topbar-text>{n_items} {n_items == 1 ? 'item' : 'items'}</list-topbar-text>}
+  </span>);
+}
 
 class TopBarContainer extends React.Component {
   constructor(props) {
     super(props);
-
-    this.back = this.back.bind(this);
-  }
-
-  back() {
-    this.props.dispatch(navigation.backBegin(routes.generate('home')));
   }
 
   render() {
-    const { n_items } = this.props;
-    const { back } = this;
+    const { n_items, dispatch } = this.props;
 
-    return <TopBar back={back} n_items={n_items} />
+    return <TopBar left={<Left dispatch={dispatch} />}
+              center={<Center />}
+              right={<Right n_items={n_items} />} />
   }
 };
 
