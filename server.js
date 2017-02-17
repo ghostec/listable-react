1e528273440d3ceabc118bab5bfdd49fd7b08294
Yuri Brito <yuriabrito@gmail.com>
2017-02-17 17:44:21 -0300

    build and server

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static(__dirname + '/build'));
+const express = require('express'),
+  compression = require('compression'),
+  bodyParser = require('body-parser'),
+  app = express();
 
 const PORT = process.env.PORT || 8080;
+
+var options = {
+  dotfiles: 'ignore',
+  etag: true,
+  extensions: ['htm', 'html'],
+  index: 'index.html',
+  lastModified: true,
+  maxAge: '1d',
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now());
+    res.header('Cache-Control', 'public, max-age=1d');
+  }
+};
+
+app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use('/', express.static(__dirname + '/build', options));
+app.use('*', express.static(__dirname + '/build', options));
 
 app.get('/.well-known/acme-challenge/:content', function(req, res) {
   res.send(process.env.CERTBOT_RESPONSE);
